117dca66aaef55e719b1d7b3c4c16452f31c706c
VLBortolotti <vitorlacobortolotti@gmail.com>
2023-01-12 21:06:25 -0300

    Add: signUpController.php

--- index.php
+++ index.php
@@ -1,9 +1,10 @@
 <?php
 require_once('database/database.php');
+require_once('dtos/response.php');
 
 $requestBody = file_get_contents('php://input');
 $requestBody = json_decode($requestBody, true) 
-              or die("Could not decode JSON");
+              or response("Could not decode JSON", 400, 0);
 
 if (array_key_exists('login', $_GET)) {
   require_once('controller/loginController.php');
