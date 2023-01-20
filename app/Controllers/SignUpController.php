86df8c85af1964944e5e8907fae2295c3a00104a
Luan Simões <timpsembs.7@gmail.com>
2023-01-20 14:11:42 -0300

    Fix sign up responses

--- app/Controllers/SignUpController.php
+++ app/Controllers/SignUpController.php
@@ -4,18 +4,18 @@ namespace App\Controllers;
 
 use PDO;
 
+use App\Modules\Response;
+
 class RegisterController
 {
     public static function post(PDO $conn, array $requestBody)
     {
         if (!isset($requestBody['cpf']) || !isset($requestBody['senha'])) {
-            $message = 'Cpf/senha não consta no corpo da requisição';
-            response($message, '400', '0');
+            return Response::error('Cpf/senha não consta no corpo da requisição');
         }
 
         if (strlen($requestBody['cpf']) < 1 || strlen($requestBody['cpf']) > 255 || strlen($requestBody['senha']) < 1 || strlen($requestBody['senha']) > 60) {
-            $message = 'CPF/senha fora do padrão esperado';
-            response($message, '400', '0');
+            return Response::error('CPF/senha fora do padrão esperado');
         }
 
         $cpf = trim($requestBody['cpf']);
@@ -29,8 +29,7 @@ class RegisterController
             $rowCount = $query->rowCount();
 
             if ($rowCount !== 0) {
-                $message = 'O CPF informado já está em uso';
-                response($message, '409', '0');
+                return Response::error('O CPF informado já está em uso');
             }
 
             $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
@@ -43,20 +42,18 @@ class RegisterController
             $rowCount = $query->rowCount();
 
             if ($rowCount === 0) {
-                $message = 'Algo deu errado - tente novamente';
-                response($message, '500', '0');
+                return Response::error('Algo deu errado - tente novamente');
             }
+
             $lastUserId = $conn->lastInsertId();
 
             $returnData = array();
             $returnData['usuario_id'] = $lastUserId;
             $returnData['cpf'] = $cpf;
 
-            $message = 'Usuário criado com sucesso';
-            response($message, '201', $returnData);
+            return Response::error('Usuário criado com sucesso');
         } catch (PDOException $ex) {
-            echo $ex->getMessage();
-            response('Erro interno', '500', '0');
+            return Response::error('Erro interno:' . $ex->getMessage());
         }
     }
 }
